2ecb1a36fd21cb9d74798b5cf625153e54932fe9
Tatiana459 <tna.sidr@gmail.com>
2021-04-14 02:13:39 +0300

    added file

---
+++ Oblic_Tovariv/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Oblic_Tovariv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.зберегтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вихідToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.зберегтиToolStripMenuItem,
+            this.вихідToolStripMenuItem});
+            this.менюToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // зберегтиToolStripMenuItem
+            // 
+            this.зберегтиToolStripMenuItem.Name = "зберегтиToolStripMenuItem";
+            this.зберегтиToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.зберегтиToolStripMenuItem.Text = "Зберегти";
+            this.зберегтиToolStripMenuItem.Click += new System.EventHandler(this.зберегтиToolStripMenuItem_Click);
+            // 
+            // вихідToolStripMenuItem
+            // 
+            this.вихідToolStripMenuItem.Name = "вихідToolStripMenuItem";
+            this.вихідToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.вихідToolStripMenuItem.Text = "Вихід";
+            this.вихідToolStripMenuItem.Click += new System.EventHandler(this.вихідToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 175);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(608, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(545, 4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(255, 94);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "where    ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Перший запис";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(169, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 46);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Останній запис";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(661, 114);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 59);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Вибірка кількості товарів по групам";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(661, 179);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 59);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Суми закупівель по постачальниках";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(661, 244);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 58);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Суми закупівель по постач. по датам";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(661, 308);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 56);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Суми продажів по покупцях за період";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(661, 370);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(118, 56);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Максимальний продаж по покупцеві за період";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Групи",
+            "Товари",
+            "Постачальники",
+            "Покупці",
+            "Закупівлі",
+            "Продажі"});
+            this.comboBox1.Location = new System.Drawing.Point(282, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.Text = "таблиці";
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(411, 104);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(109, 51);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Подивитись таблицю";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(282, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(245, 96);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.Location = new System.Drawing.Point(526, 104);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(129, 51);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "Запит";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.Location = new System.Drawing.Point(182, 0);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(94, 42);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "Очистити";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Облік руху товарів";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem зберегтиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вихідToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ Oblic_Tovariv/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+
+namespace Oblic_Tovariv
+{
+    public partial class Form1 : Form
+    {
+        string connectString = @"Data Source=.\SQLEXPRESS;
+Initial Catalog=Облік руху товарів;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void вихідToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Вийти з програми?", " ", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            { this.Close(); }
+        }
+
+        public void Zapit(string Sql_Z)
+        {
+            try
+            {
+                string sql = Sql_Z;
+                using (SqlConnection connection = new SqlConnection(connectString))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    dataGridView1.DataSource = ds.Tables[0];
+                    connection.Close();
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        private void зберегтиToolStripMenuItem_Click(object sender, EventArgs e)
+        { DialogResult result = MessageBox.Show("Зберегти дані в файл?", " ", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            { SaveFileDialog savefile = new SaveFileDialog();
+                savefile.DefaultExt = ".txt";
+                savefile.Filter = "test files|*.txt";
+                if (savefile.ShowDialog() == System.Windows.Forms.DialogResult.OK && savefile.FileName.Length > 0)
+                {
+                    using (StreamWriter sw = new StreamWriter(savefile.FileName, true))
+                    {
+                        try
+                        {
+                            for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                            {
+                                for (int i = 0; i < dataGridView1.Rows[j].Cells.Count; i++)
+                                { sw.Write(dataGridView1.Rows[j].Cells[i].Value + " "); }
+                                sw.WriteLine();
+                            }
+                            sw.Close();
+                            MessageBox.Show("Файл успішно збережений");
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Помилка при збережені файла!");
+                            sw.Close();
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sel = comboBox1.SelectedItem.ToString();
+                if (sel == "Групи")
+                { Zapit("SELECT* from Групи"); }
+                else if (sel == "Товари")
+                { Zapit("SELECT* from Товари"); }
+                else if (sel == "Постачальники")
+                { Zapit("SELECT* from Постачальники"); }
+                else if (sel == "Покупці")
+                { Zapit("SELECT* from Покупці"); }
+                else if (sel == "Закупівлі")
+                { Zapit("SELECT* from Закупівлі"); }
+                else if (sel == "Продажі")
+                { Zapit("SELECT* from Продажі"); }
+                else { MessageBox.Show("Невірний вибір"); }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentRow.Index;
+            if (index != 0)
+            {
+                dataGridView1.Rows[index].Selected = true;
+                dataGridView1.CurrentCell = dataGridView1[0, 0];
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentRow.Index;
+           if( index!= dataGridView1.Rows.Count)
+                {
+                dataGridView1.Rows[index].Selected = true;
+                dataGridView1.CurrentCell = dataGridView1[0, dataGridView1.Rows.Count-1];
+                }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = "Select Г.Id_Gr as Група, Г.Name_Gr as Назва, З.Kolichestvo from Групи as Г join Закупівлі as З" +
+                       " on З.Id_Gr=Г.Id_Gr join Товари as Т on Т.Id_Tov=З.Id_Tov " + richTextBox2.Text + " group by Г.Id_Gr,Г.Name_Gr,З.Kolichestvo";
+                Zapit(richTextBox1.Text);
+                comboBox1.Text = "Групи";
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = "Select П.FIO as Постачальник, sum(З.Price_Z) as Сума from Постачальники as П join Закупівлі as З" +
+                       " on З.Id_Post=П.Id_Post " + richTextBox2.Text + " group by П.FIO";
+                Zapit(richTextBox1.Text);
+                comboBox1.Text = "Постачальники";
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = "Select П.FIO as Постачальник, З.Data_Zak as Дата_закупки, sum(З.Price_Z*З.Kolichestvo) as Сума from Постачальники as П join Закупівлі as З" +
+                       " on З.Id_Post=П.Id_Post " + richTextBox2.Text + " group by П.FIO, З.Data_Zak";
+                Zapit(richTextBox1.Text);
+                comboBox1.Text = "Закупівлі";
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = "Select П.FIO_Pok as Покупець, (sum(Пр.Price_Pr)*Пр.Kolichestvo_Pr) " +
+       "as Сума_Продажі from Покупці as П join Продажі as Пр on (Пр.Id_Pok=П.Id_Pok and Data_Pr between \'16.09.2019\' and \'1.11.2019\' ) "
+          + richTextBox2.Text + " group by П.FIO_Pok, Пр.Kolichestvo_Pr";
+                Zapit(richTextBox1.Text);
+                comboBox1.Text = "Продажі";
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = "Select П.FIO_Pok as Покупець, max(Пр.Price_Pr*Пр.Kolichestvo_Pr) " +
+       "as Сума_Продажі from Покупці as П join Продажі as Пр on (Пр.Id_Pok=П.Id_Pok and Data_Pr between \'16.09.2019\' and \'1.11.2019\' ) "
+          + richTextBox2.Text + " group by П.FIO_Pok";
+                Zapit(richTextBox1.Text);
+                comboBox1.Text = "Продажі";
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Zapit(richTextBox1.Text);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+    }
+    }
+
+ 
+ 
+
+ 
